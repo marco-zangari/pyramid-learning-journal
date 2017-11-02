4e254a808d0dffc4baf3cf18a84199465c50c65c
Marco Zangari <mjzangari@gmail.com>
2017-11-01 20:24:36 -0700

    Added the tox.ini

--- learning_journal/tests.py
+++ learning_journal/tests.py
@@ -24,18 +24,24 @@ def test_list_view_response_status_200(dummy_request):
 
 
 def test_detail_view_response_status_200(dummy_request):
-    """Test the list_view function to return a status 200 OK."""
+    """Test the detail_view function to return a status 200 OK."""
     response = list_view(dummy_request)
     assert response.status_code == 200
 
 
 def test_create_view_response_status_200(dummy_request):
-    """Test the list_view function to return a status 200 OK."""
+    """Test the create_view function to return a status 200 OK."""
     response = list_view(dummy_request)
     assert response.status_code == 200
 
 
-def test_list_update_response_status_200(dummy_request):
-    """Test the list_view function to return a status 200 OK."""
+def test_update_view_response_status_200(dummy_request):
+    """Test the update_view function to return a status 200 OK."""
     response = list_view(dummy_request)
     assert response.status_code == 200
+
+
+def test_list_view_response_text_has_proper_content_type(dummy_request):
+    """Test that list_view returns content expected."""
+    response = list_view(dummy_request)
+    assert response.content_type = 'text/html'
