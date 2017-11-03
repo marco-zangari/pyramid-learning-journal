77775e486105e9016aec1d55a0470f2539311b97
Marco Zangari <mjzangari@gmail.com>
2017-11-03 16:39:53 -0700

    working through step2

--- learning_journal/__init__.py
+++ learning_journal/__init__.py
@@ -5,7 +5,6 @@ def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application."""
     config = Configurator(settings=settings)
     config.include('pyramid_jinja2')
-    config.include('.views')
     config.include('.routes')
     config.scan()
     return config.make_wsgi_app()
--- learning_journal/views/default.py
+++ learning_journal/views/default.py
@@ -1,12 +1,13 @@
 """Set the default for the application."""
 
-from pyramid.response import Response
+from pyramid.response import view_config
 import io
 import os
 
 HERE = os.path.dirname(__file__)
 
 
+@view_config(route_name='list_view', renderer='learning_journal:templates/list_view.jinja2')
 def list_view(request):
     """Create a home page."""
     path = os.path.join(HERE, '../templates/list_view.html')
@@ -14,6 +15,7 @@ def list_view(request):
         return Response(imported_text.read())
 
 
+@view_config(route_name='create_view', renderer='learning_journal:templates/create_view.jinja2')
 def create_view(request):
     """Create a new blog post."""
     path = os.path.join(HERE, '../templates/create_view.html')
@@ -21,6 +23,7 @@ def create_view(request):
         return Response(imported_text.read())
 
 
+@view_config(route_name='detail_view', renderer='learning_journal:templates/detail_view.jinja2')
 def detail_view(request):
     """Show single blog post."""
     path = os.path.join(HERE, '../templates/detail_view.html')
@@ -28,6 +31,7 @@ def detail_view(request):
         return Response(imported_text.read())
 
 
+@view_config(route_name='update_view', renderer='learning_journal:templates/update_view.jinja2')
 def update_view(request):
     """Show single blog post."""
     path = os.path.join(HERE, '../templates/update_view.html')
