3ed6063a448e03650d1a8b6ff5914c03570ea0ae
Marco Zangari <mjzangari@gmail.com>
2017-11-02 19:44:28 -0700

    Refactored SetUp.py Removed lines from setup.py that were not working. Removed an additional setup.py.

--- learning_journal/views/default.py
+++ learning_journal/views/default.py
@@ -11,25 +11,25 @@ def list_view(request):
     """Create a home page."""
     path = os.path.join(HERE, '../templates/list_view.html')
     with io.open(path) as imported_text:
-        return Response(imported_text.read)
+        return Response(imported_text.read())
 
 
 def create_view(request):
     """Create a new blog post."""
     path = os.path.join(HERE, '../templates/create_view.html')
     with io.open(path) as imported_text:
-        return Response(imported_text.read)
+        return Response(imported_text.read())
 
 
 def detail_view(request):
     """Show single blog post."""
     path = os.path.join(HERE, '../templates/detail_view.html')
     with io.open(path) as imported_text:
-        return Response(imported_text.read)
+        return Response(imported_text.read())
 
 
 def update_view(request):
     """Show single blog post."""
     path = os.path.join(HERE, '../templates/update_view.html')
     with io.open(path) as imported_text:
-        return Response(imported_text.read)
+        return Response(imported_text.read())
