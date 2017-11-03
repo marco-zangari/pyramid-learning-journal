2820156d521e0a9725980cc9658c7a0e850330f4
Marco Zangari <mjzangari@gmail.com>
2017-11-03 14:05:11 -0700

    Added routes to init.py in the views folder

--- learning_journal/views/__init__.py
+++ learning_journal/views/__init__.py
@@ -3,5 +3,7 @@
 
 def includeme(config):
     """Add views for each view and provide route name."""
-    pass
-    config.add
+    config.add_view(list_view, route_name='home')
+    config.add_view(detail_view, route_name='journal')
+    config.add_view(create_view, route_name='new-entry')
+    config.add_view(update_view, route_name='edit-entry')
