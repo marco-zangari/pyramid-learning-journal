a1246075ef19b5a6d9ed810f990eeae55c1fa69f
Marco Zangari <mjzangari@gmail.com>
2017-11-02 20:00:25 -0700

    Tests.py Refactored the tests.py tests to take advantage of the new list_view_fixture

--- learning_journal/tests.py
+++ learning_journal/tests.py
@@ -44,26 +44,25 @@ def test_list_view_response_status_200(list_view_fixture):
     assert response.status_code == 200
 
 
-# def test_detail_view_response_status_200(dummy_request):
-#     """Test the detail_view function to return a status 200 OK."""
-#     response = detail_view(dummy_request)
-#     assert response.status_code == 200
+def test_detail_view_response_status_200(list_view_fixture):
+    """Test the detail_view function to return a status 200 OK."""
+    response = list_view_fixture
+    assert response.status_code == 200
 
 
-# def test_create_view_response_status_200(dummy_request):
-#     """Test the create_view function to return a status 200 OK."""
-#     response = create_view(dummy_request)
-#     assert response.status_code == 200
+def test_create_view_response_status_200(list_view_fixture):
+    """Test the create_view function to return a status 200 OK."""
+    response = list_view_fixture
+    assert response.status_code == 200
 
 
-# def test_update_view_response_status_200(dummy_request):
-#     """Test the update_view function to return a status 200 OK."""
-#     response = update_view(dummy_request)
-#     assert response.status_code == 200
+def test_update_view_response_status_200(list_view_fixture):
+    """Test the update_view function to return a status 200 OK."""
+    response = list_view_fixture
+    assert response.status_code == 200
 
 
-# def test_list_view_response_text_has_proper_content_type(dummy_request):
-#     """Test that list_view returns content expected."""
-#     response = list_view(dummy_request)
-#     import pdb; pdb.set_trace()
-#     assert response.content_type == 'text/html'
+def test_list_view_response_text_has_proper_content_type(list_view_fixture):
+    """Test that list_view returns content expected."""
+    response = list_view_fixture
+    assert response.content_type == 'text/html'
