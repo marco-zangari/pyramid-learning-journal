792a5c311884f8f18e65a1b6e5a0f88b26d1001a
Marco Zangari <mjzangari@gmail.com>
2017-11-03 18:04:55 -0700

    server running on localhost:6543

--- learning_journal/views/default.py
+++ learning_journal/views/default.py
@@ -1,30 +1,27 @@
 """Set the default for the application."""
 
-from pyramid.response import view_config
-
-
-HERE = os.path.dirname(__file__)
+from pyramid.view import view_config
 
 
 @view_config(route_name='list_view', renderer='learning_journal:templates/list_view.jinja2')
 def list_view(request):
     """Create a home page."""
-        return {}
+    return {}
 
 
 @view_config(route_name='create_view', renderer='learning_journal:templates/create_view.jinja2')
 def create_view(request):
     """Create a new blog post."""
-        return {}
+    return {}
 
 
 @view_config(route_name='detail_view', renderer='learning_journal:templates/detail_view.jinja2')
 def detail_view(request):
     """Show single blog post."""
-        return {}
+    return {}
 
 
 @view_config(route_name='update_view', renderer='learning_journal:templates/update_view.jinja2')
 def update_view(request):
     """Show single blog post."""
-        return {}
+    return {}
