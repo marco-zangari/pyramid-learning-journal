df959416f02736c5f71d659f0d2c697438a3d426
Marco Zangari <mjzangari@gmail.com>
2017-11-04 18:44:53 -0700

    ran test for isinstance on list_view

--- learning_journal/tests.py
+++ learning_journal/tests.py
@@ -5,7 +5,7 @@ from pyramid import testing
 import pytest
 
 
-from learning_journal.views.default import(
+from learning_journal.views.default import (
     list_view,
     detail_view,
     create_view,
@@ -20,49 +20,38 @@ def list_view_fixture():
     return list_view(lv)
 
 
-# @pytest.fixture
-# def dummy_request():
-#     """Make dummy request fixture."""
-#     return testing.DummyRequest()
+def test_list_view():
+    """Test that what's returned by the view is a dictionary of values."""
+    request = testing.DummyRequest()
+    info = list_view(request)
+    assert isinstance(info, dict)
 
 
-# @pytest.fixture
-# def dummy_request():
-#     """Make dummy request fixture."""
-#     return testing.DummyRequest()
+# def test_list_view_response_status_200(list_view_fixture):
+#     """Test the list_view function to return a status 200 OK."""
+#     response = list_view_fixture
+#     assert response.status_code == 200
 
 
-# @pytest.fixture
-# def dummy_request():
-#     """Make dummy request fixture."""
-#     return testing.DummyRequest()
+# def test_detail_view_response_status_200(list_view_fixture):
+#     """Test the detail_view function to return a status 200 OK."""
+#     response = list_view_fixture
+#     assert response.status_code == 200
 
 
-def test_list_view_response_status_200(list_view_fixture):
-    """Test the list_view function to return a status 200 OK."""
-    response = list_view_fixture
-    assert response.status_code == 200
+# def test_create_view_response_status_200(list_view_fixture):
+#     """Test the create_view function to return a status 200 OK."""
+#     response = list_view_fixture
+#     assert response.status_code == 200
 
 
-def test_detail_view_response_status_200(list_view_fixture):
-    """Test the detail_view function to return a status 200 OK."""
-    response = list_view_fixture
-    assert response.status_code == 200
+# def test_update_view_response_status_200(list_view_fixture):
+#     """Test the update_view function to return a status 200 OK."""
+#     response = list_view_fixture
+#     assert response.status_code == 200
 
 
-def test_create_view_response_status_200(list_view_fixture):
-    """Test the create_view function to return a status 200 OK."""
-    response = list_view_fixture
-    assert response.status_code == 200
-
-
-def test_update_view_response_status_200(list_view_fixture):
-    """Test the update_view function to return a status 200 OK."""
-    response = list_view_fixture
-    assert response.status_code == 200
-
-
-def test_list_view_response_text_has_proper_content_type(list_view_fixture):
-    """Test that list_view returns content expected."""
-    response = list_view_fixture
-    assert response.content_type == 'text/html'
+# def test_list_view_response_text_has_proper_content_type(list_view_fixture):
+#     """Test that list_view returns content expected."""
+#     response = list_view_fixture
+#     assert response.content_type == 'text/html'
