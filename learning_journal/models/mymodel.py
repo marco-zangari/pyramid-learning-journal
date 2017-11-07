f5b8aa739de95b082b8446ea1ab02e495bb428a8
Marco Zangari <mjzangari@gmail.com>
2017-11-07 13:17:03 -0800

    added Index line to mymodel.py in learning_journal.

--- learning_journal/models/mymodel.py
+++ learning_journal/models/mymodel.py
@@ -17,3 +17,5 @@ class Journal(Base):
     title = Column(Unicode)
     body = Column(Unicode)
     creation_date = Column(DateTime)
+
+Index('journal_index', Journal.name, unique=True, mysql_length=255)
