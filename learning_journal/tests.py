05f40aa5ece95195891e19d9f8206738badbf01b
Marco Zangari <mjzangari@gmail.com>
2017-11-09 17:03:21 -0800

    added first unit test of list_view. commented out the tests from step2

--- learning_journal/tests.py
+++ learning_journal/tests.py
@@ -13,43 +13,52 @@ from learning_journal.views.default import (
     update_view,
 )
 
+def test_model_gets_added(db_session):
+    assert len(db_session.query(Journal).all()) == 0
+    model = Journal(
+        title="Some description text",
+        body="Some longer description than in the title",
+        creation_date=datetime.datetime.now(),
+    )
+    db_session.add(model)
+    assert len(db_session.query(Journal).all()) == 1
 
-@pytest.fixture
-def list_view_fixture():
-    """Make dummy request fixture."""
-    lv = testing.DummyRequest()
-    return list_view(lv)
-
-
-def test_list_view():
-    """Test that what's returned by the view is a dictionary of values."""
-    request = testing.DummyRequest()
-    info = list_view(request)
-    assert isinstance(info, dict)
-
-
-def test_list_view_returns_proper_amount_of_content():
-    """Home view response has file content."""
-    request = testing.DummyRequest()
-    response = list_view(request)
-    assert len(response['entries']) == len(ENTRIES)
+# @pytest.fixture
+# def list_view_fixture():
+#     """Make dummy request fixture."""
+#     lv = testing.DummyRequest()
+#     return list_view(lv)
 
 
-def test_detail_view():
-    """Test that what's returned by the view is a dictionary of values."""
-    request = testing.DummyRequest()
-    request.matchdict['id'] = 13
-    info = detail_view(request)
-    assert isinstance(info, dict)
+# def test_list_view():
+#     """Test that what's returned by the view is a dictionary of values."""
+#     request = testing.DummyRequest()
+#     info = list_view(request)
+#     assert isinstance(info, dict)
 
 
-def test_detail_view_response_contains_entries_attrs():
-    """Test that what's returned by the view contains one expense object."""
-    request = testing.DummyRequest()
-    request.matchdict['id'] = 13
-    info = detail_view(request)
-    for key in ["creation_date", "id", "body", "title"]:
-        assert key in info['entry'].keys()
+# def test_list_view_returns_proper_amount_of_content():
+#     """Home view response has file content."""
+#     request = testing.DummyRequest()
+#     response = list_view(request)
+#     assert len(response['entries']) == len(ENTRIES)
+
+
+# def test_detail_view():
+#     """Test that what's returned by the view is a dictionary of values."""
+#     request = testing.DummyRequest()
+#     request.matchdict['id'] = 13
+#     info = detail_view(request)
+#     assert isinstance(info, dict)
+
+
+# def test_detail_view_response_contains_entries_attrs():
+#     """Test that what's returned by the view contains one expense object."""
+#     request = testing.DummyRequest()
+#     request.matchdict['id'] = 13
+#     info = detail_view(request)
+#     for key in ["creation_date", "id", "body", "title"]:
+#         assert key in info['entry'].keys()
 
 
 # def test_list_view_response_status_200(list_view_fixture):
